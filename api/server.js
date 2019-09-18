a5706b89e5e4240b896c227645fa44414d426136
yannisoo <yannis.b8@gmail.com>
2019-09-18 20:00:54 +0200

    step 13 fini

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ cors = require('cors'),
 mongoose = require('mongoose'),
 config = require('./DB');
 
+const productRoute = require('./routes/product.route');
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
   () => {console.log('Database is connected') },
@@ -15,6 +16,7 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/products', productRoute);
 const port = process.env.PORT || 4000;
 
 const server = app.listen(port, function(){
